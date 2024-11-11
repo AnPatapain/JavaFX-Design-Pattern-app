e0a169fca212b88066b56af051eb2693e4b9f5c3
kean <nkalk192002@gmail.com>
2024-11-11 18:32:05 +0100

    Example of Docs for Phuoc

--- src/main/java/fr/insa/bourges/firstapplicationjfx/base/event/EventDispatcher.java
+++ src/main/java/fr/insa/bourges/firstapplicationjfx/base/event/EventDispatcher.java
@@ -1,21 +1,37 @@
 /**
- * EventDispatcher allows to subscribe different event type with associated event listener
+ * The EventDispatcher interface represents the subject in the Observer pattern.
+ * It manages the subscription of multiple EventListeners to various EventTypes
+ * and is responsible for dispatching events to the appropriate listeners.
+ * EventListeners can subscribe to specific EventTypes via this interface, and
+ * the EventDispatcher will notify them when events of those types occur.
+ *
  * Author: Ke An NGUYEN
  */
-
 package fr.insa.bourges.firstapplicationjfx.base.event;
 
 public interface EventDispatcher {
+
     /**
-     * One EventListener can listen to different event types
-     * @param eventListener
-     * @param eventTypes
+     * Subscribes an EventListener to one or more EventTypes.
+     *
+     * This method allows an EventListener to register itself for specific types
+     * of events. When any of these events occur, the EventDispatcher will notify
+     * the subscribed EventListener by calling its handleEvent method.
+     *
+     * @param eventListener The EventListener to subscribe to the specified event types.
+     * @param eventTypes    The types of events the listener wants to subscribe to.
      */
     void subscribe(EventListener eventListener, EventType... eventTypes);
 
     /**
-     * Dispatch the eventType. The impl of this method may call the handleEvent of the eventListeners.
-     * @param eventType
+     * Dispatches an event of the specified EventType.
+     *
+     * This method notifies all EventListeners that have subscribed to the specified
+     * EventType by calling their handleEvent method. The implementation of this method
+     * may maintain a mapping between event types and their associated listeners to
+     * efficiently route the event to the correct listeners.
+     *
+     * @param eventType The type of event to dispatch.
      */
     void dispatchEvent(EventType eventType);
 }
--- src/main/java/fr/insa/bourges/firstapplicationjfx/base/event/EventListener.java
+++ src/main/java/fr/insa/bourges/firstapplicationjfx/base/event/EventListener.java
@@ -1,12 +1,36 @@
 /**
- * Subscribe to the EventDispatcher and handle the dispatched event (from EventDispatcher) based
- * on the event type
+ * The EventListener interface represents an observer in the Observer pattern.
+ * It allows classes implementing this interface to subscribe to an EventDispatcher
+ * and respond to dispatched events based on event type.
+ *
  * Author: Ke An NGUYEN
  */
 package fr.insa.bourges.firstapplicationjfx.base.event;
 
 public interface EventListener {
+
+    /**
+     * Sets up the subscription of this EventListener to an EventDispatcher.
+     *
+     * This method allows the EventListener to determine which events it should
+     * subscribe to by directly interacting with the EventDispatcher.
+     * Unlike a direct subscription by the EventDispatcher, this approach gives
+     * the EventListener control over which specific events it listens to, making
+     * it suitable for cases where the EventDispatcher doesn't know the precise events
+     * an EventListener is interested in.
+     *
+     * @param eventDispatcher The EventDispatcher to which this listener will subscribe.
+     */
     void setSubscription(EventDispatcher eventDispatcher);
 
+    /**
+     * Handles an event dispatched by the EventDispatcher.
+     *
+     * This method is called when an event of a type that the EventListener is subscribed
+     * to is dispatched by the EventDispatcher. Implementing classes should define how
+     * they handle each specific EventType in this method.
+     *
+     * @param eventType The type of event that was dispatched, which this listener is handling.
+     */
     void handleEvent(EventType eventType);
 }
